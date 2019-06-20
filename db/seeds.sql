9838ba61d6b529e349e87faf99b39fbb95a3ca9a
Geeyan <geeyan.bootcamp@gmail.com>
2019-06-20 18:01:30 -0400

    burger logger homework but did chicken wings instead

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO cats (name) VALUES ('Buffalo Wings');
+INSERT INTO cats (name) VALUES ('Medium Spicy Wings');
+INSERT INTO cats (name, sleepy) VALUES ('Plain Wings', true);
+INSERT INTO cats (name, sleepy) VALUES ('Ginger Wings', true);
+INSERT INTO cats (name, sleepy) VALUES ('Mild Wings', true);
+INSERT INTO cats (name) VALUES ('Burnt Wings');
